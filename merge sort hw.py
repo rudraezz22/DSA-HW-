6e1adb3d4d18c8bc973d196c25fa58db23f5de83
rudraezz22 <proop500@gmail.com>
2025-12-12 12:21:12 +0530

    Add files via upload

---
+++ merge sort hw.py
@@ -0,0 +1,30 @@
+def mergesort(A):
+  if len(A)>1:
+    mid = len(A)//2
+    left = A[:mid]
+    right = A[mid:]
+
+    mergesort(left)
+    mergesort(right)
+
+    i=j=k=0
+    while i<len(left) and j<len(right):
+        if left[i] < right[j]:
+            A[k] = left[i]
+            i+=1
+        else:
+            A[k] = right[j]
+            j+=1
+        k+=1
+    while i<len(left):
+        A[k] = left[i]
+        i+=1
+        k+=1
+    while j<len(right):
+        A[k] = right[j]
+        j+=1
+        k+=1
+A = list(map(int, input("Enter values of list separated by space: ").split()))
+
+mergesort(A)
+print(A)
